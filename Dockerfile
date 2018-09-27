86568f4692775452ab0a3e86d8272af69e2a0f7f
Cristian Fernandez <cfernandez@Cris-MacBook.local>
2018-09-27 21:36:06 +0200

    skeleton of Newsletter Subscription Service

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM java:8
+FROM maven:alpine
+
+WORKDIR /app
+
+COPY . /app
+
+RUN mvn -v
+EXPOSE 8080
+
+LABEL maintainer="cristian.fernandez@gmx.de"
+ADD ./target/newsletter-subscription-service-0.0.1-SNAPSHOT.jar newsletter-subscription-service-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","newsletter-subscription-service-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/eurowings/newslettersubscriptionservice/NewsletterSubscriptionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.eurowings.newslettersubscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterSubscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsletterSubscriptionServiceApplication.class, args);
+    }
+}
